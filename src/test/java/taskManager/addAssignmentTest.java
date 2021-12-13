7cd374d1f74c0f26a6692c9cadc91fd512502ccd
Draper, Benjamin John (894699051) <bjdraper@wpi.edu>
2021-12-13 01:04:31 -0500

    viewing assignees

--- src/test/java/taskManager/addAssignmentTest.java
+++ src/test/java/taskManager/addAssignmentTest.java
@@ -7,7 +7,7 @@ import taskManager.http.assignResponse;
 public class addAssignmentTest extends LambdaTest{
     @Test
     public void testAddAssignment(){
-        assignRequest ar = new assignRequest("Steve Jobs", "2: Make another task", "tp3");
+        assignRequest ar = new assignRequest("teammate1", "1: Task 1", "newestProj");
         assignResponse resp = new assignmentHandler().handleRequest(ar, createContext("create"));
         Assert.assertEquals(400, resp.httpCode);
     }
