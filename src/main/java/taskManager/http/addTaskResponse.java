146d5c016b5f4d971b156ee2391f752af98b9209
soysauce7777 <squinnw@gmail.com>
2021-12-14 15:11:48 -0500

    Deleting assignments doesn't depend on database state now

--- src/main/java/taskManager/http/addTaskResponse.java
+++ src/main/java/taskManager/http/addTaskResponse.java
@@ -3,20 +3,27 @@ package taskManager.http;
 public class addTaskResponse {
     public final String name;
     public final String projectName;
+    public final String taskID;
     public final int httpCode;
     public final String error;
 
-    public addTaskResponse(String s, String p, int code){
+    public addTaskResponse(String s, String p, String t, int code){
         this.name = s;
         this.projectName = p;
+        this.taskID = t;
         this.httpCode = code;
         this.error = "";
     }
     public addTaskResponse(String errorMessage, int code){
         this.error = errorMessage;
         this.httpCode = code;
+        this.taskID = "";
         this.name = "";
         this.projectName = "";
     }
     public String toString(){return "Task("+name+":"+projectName+")";}
+
+    public String getTaskID(){
+        return this.taskID;
+    }
 }
